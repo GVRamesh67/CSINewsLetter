43a89d4c4a548224c1103a9a9ccdb013a65d0da3
GVRamesh67 <gvramesh67@gmail.com>
2017-09-26 17:47:40 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ w3.includeHTML();
 var app = angular.module("csiApp", ['ngAnimate']);
 app.controller('csiCtrl', function ($scope, $filter, $http, $location, $anchorScroll) {
     $scope.boolHead = true;
+    $scope.boolEditerNote = true;
+    $scope.boolChapterAct = true;
+    $scope.boolFutureAct = true;
+    $scope.boolHotTrends = true;
+    $scope.boolTechPaper = true;
     $scope.getEditorNote = function () {
         var req = new Req('data/editorNote.json', 'GET');
         $http(req).then(
@@ -16,6 +21,13 @@ app.controller('csiCtrl', function ($scope, $filter, $http, $location, $anchorSc
                     $scope.chaptAct = response.data;
                 });
     };
+    $scope.getFutureAct = function () {
+        var req = new Req('data/futureActivities.json', 'GET');
+        $http(req).then(
+                function (response) {
+                    $scope.futureAct = response.data;
+                });
+    };
     $scope.getTechnologyLinks = function () {
         var req = new Req('data/technologyLinks.json', 'GET');
         $http(req).then(
@@ -26,4 +38,5 @@ app.controller('csiCtrl', function ($scope, $filter, $http, $location, $anchorSc
     $scope.getEditorNote();
     $scope.getChapterAct();
     $scope.getTechnologyLinks();
+    $scope.getFutureAct();
 });
